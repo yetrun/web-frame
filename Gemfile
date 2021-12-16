f6b3bacc34455c29ac8fdff23f59930ab69bd026
yetrun <89437145+yetrun@users.noreply.github.com>
2021-12-16 14:51:01 +0800

    添加测试框架：Minitest

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://gems.ruby-china.com/'
 
+group :test do
+  gem 'minitest'
+end
+
 group :test, :development do
   gem 'pry-byebug'
 end
---
+++ demo_test.rb
@@ -0,0 +1,29 @@
+require "minitest/autorun"
+
+class Meme
+  def i_can_has_cheezburger?
+    "OHAI!"
+  end
+
+  def will_it_blend?
+    "YES!"
+  end
+end
+
+class TestMeme < Minitest::Test
+  def setup
+    @meme = Meme.new
+  end
+
+  def test_that_kitty_can_eat
+    assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+  end
+
+  def test_that_it_will_not_blend
+    refute_match /^no/i, @meme.will_it_blend?
+  end
+
+  def test_that_will_be_skipped
+    skip "test this later"
+  end
+end
