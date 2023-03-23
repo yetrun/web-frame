f81de9d189ff35145f0d47504c09b8b01f23c840
yetrun <89437145+yetrun@users.noreply.github.com>
2023-03-23 09:37:04 +0800

    整理测试用例

--- spec/json_schema/schema_builder_spec.rb
+++ spec/json_schema/schema_builder_spec.rb
@@ -4,62 +4,304 @@
 require 'spec_helper'
 
 describe 'Schema Builders' do
-  xit '属于数组的属性要保留' do
-    schema = Meta::JsonSchema::SchemaBuilderTool.build do
-      property :array_value, type: 'array', using: Class.new(Meta::Entity), value: proc { |board| [] }
-    end.to_schema
-
-    array_schema = schema.properties[:array_value].schema
-    expect(array_schema.items.is_a?(Meta::JsonSchema::ObjectSchema)).to be true
-    expect(array_schema.options.include?(:value)).to be true
-  end
-
-  it '使用 scope 约束 Schema' do
-    builder = Meta::JsonSchema::ObjectSchemaBuilder.new do
-      property :xxx, scope: 'xxx'
-      property :yyy, scope: 'yyy'
-      property :zzz
+  describe 'build array schema' do
+    it '属于数组的属性要保留' do
+      array_schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'array', using: Class.new(Meta::Entity), value: proc { |board| [] }
+      expect(array_schema.options[:value]).to be_a(Proc)
     end
-
-    schema = builder.lock_scope('xxx').to_schema
-
-    value = schema.filter({})
-    expect(value.keys).to eq([:xxx, :zzz]) # 相当于自动传递 scope: 'xxx'
-
-    value = schema.filter({}, scope: 'yyy')
-    expect(value.keys).to eq([:xxx, :zzz]) # 忽略用户传递的 scope 选项
   end
 
-  it '使用 scope 约束 Schema：内部模块依然有用' do
-    builder = Meta::JsonSchema::ObjectSchemaBuilder.new do
-      property :xxx, scope: 'xxx'
-      property :yyy, scope: 'yyy'
-      property :zzz do
-        property :xx, scope: 'xxx'
-        property :yy, scope: 'yyy'
+  describe 'ObjectSchemaBuilder' do
+    describe 'lock scope' do
+      let(:builder) do
+        Meta::JsonSchema::ObjectSchemaBuilder.new do
+          property :xxx, scope: 'xxx'
+          property :yyy, scope: 'yyy'
+          property :zzz
+        end
+      end
+      let(:normal_schema) { builder.to_schema }
+      let(:scoped_schema) { builder.lock_scope('xxx').to_schema }
+      let(:input_value) { { 'xxx' => 'xxx', 'yyy' => 'yyy', 'zzz' => 'zzz' } }
+
+      it '当不传递 scope 时，调用 filter 相当于传递了锁住的 scope' do
+        expect(
+          scoped_schema.filter(input_value)
+        ).to eq(
+          normal_schema.filter(input_value, scope: 'xxx')
+        )
+      end
+
+      it '即使传递了其他的 scope，调用 filter 仍相当于传递了锁住的 scope' do
+        expect(
+          scoped_schema.filter(input_value, scope: 'yyy')
+        ).to eq(
+          normal_schema.filter(input_value, scope: 'xxx')
+        )
+      end
+
+      context '定义更深层次' do
+        let(:builder) {
+          Meta::JsonSchema::ObjectSchemaBuilder.new do
+            property :xxx, scope: 'xxx'
+            property :yyy, scope: 'yyy'
+            property :zzz do
+              property :xx, scope: 'xxx'
+              property :yy, scope: 'yyy'
+            end
+          end
+        }
+        let(:input_value) { { 'xxx' => 'xxx', 'yyy' => 'yyy', 'zzz' => { 'xx' => 'xx', 'yy' => 'yy' } } }
+
+        it '对内部模块仍然有用' do
+          expect(
+            scoped_schema.filter(input_value)
+          ).to eq(
+            normal_schema.filter(input_value, scope: 'xxx')
+          )
+        end
+      end
+    end
+    describe 'lock exclude' do
+      let(:builder) do
+        Meta::JsonSchema::ObjectSchemaBuilder.new do
+          property :xxx
+          property :yyy
+        end
+      end
+      let(:normal_schema) { builder.to_schema }
+      let(:excluded_schema) { builder.lock_exclude([:yyy]).to_schema }
+      let(:input_value) { { 'xxx' => 'xxx', 'yyy' => 'yyy' } }
+
+      it '当不传递 exclude 时，相当于传递了锁住的 exclude' do
+        expect(
+          excluded_schema.filter(input_value)
+        ).to eq(
+          normal_schema.filter(input_value, exclude: [:yyy])
+        )
+      end
+
+      it '当传递了 exclude 时，仍相当于传递了锁住的 exclude' do
+        expect(
+          excluded_schema.filter(input_value, exclude: [:xxx])
+        ).to eq(
+          normal_schema.filter(input_value, exclude: [:yyy])
+        )
+      end
+    end
+  end
+
+  describe 'options' do
+    describe 'default:' do
+      it 'boolean 类型的默认值设置工作正常' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'boolean', default: false
+
+        value = schema.filter(nil)
+        expect(value.class).to be(FalseClass)
       end
     end
 
-    schema = builder.lock_scope('xxx').to_schema
+    describe 'value:' do
+      it '调用 `value` 时父级对象不存在对应键值不会抛出异常' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build do
+          property :foo, type: 'string', value: lambda { 'foo' }
+        end.to_schema
 
-    value = schema.filter({ 'zzz' => {} })
-    expect(value).to eq({
-      xxx: nil,
-      zzz: {
-        xx: nil
-      }
-    }) # 相当于自动传递 scope: 'xxx'
-  end
+        expect {
+          schema.filter(Object.new)
+        }.not_to raise_error
+      end
 
-  it '使用 exclude 约束 Schema' do
-    builder = Meta::JsonSchema::ObjectSchemaBuilder.new do
-      property :xxx
-      property :yyy
+      it '调用 `value` 时块接受父级传值' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build do
+          property :foo, type: 'string', value: lambda { |parent| parent['bar'] }
+        end.to_schema
+
+        expect(
+          schema.filter('bar' => 'foo' )
+        ).to eq({ foo: 'foo' })
+      end
+
+      it '调用 `value` 时可在指定的环境下执行' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build do
+          property :foo, type: 'string', value: lambda { resolve_foo }
+        end.to_schema
+
+        object = Object.new
+        def object.resolve_foo; 'foo' end
+
+        expect(
+          schema.filter(object, execution: object)
+        ).to eq({ foo: 'foo' })
+      end
+
+      it '对于根级别的定义，调用 `value` 时块也能正常执行' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'string', value: lambda { 'foo' }
+
+        expect(
+          schema.filter('bar')
+        ).to eq('foo')
+      end
     end
 
-    schema = builder.lock_exclude([:yyy]).to_schema
+    describe 'validators' do
+      describe 'format:' do
+        it 'nil 值不会触发 format validator' do
+          schema = Meta::JsonSchema::SchemaBuilderTool.build do
+            property :number, type: 'string', format: /\d+/
+          end
 
-    value = schema.filter({})
-    expect(value.keys).to eq([:xxx])
+          expect {
+            schema.filter(number: nil)
+          }.not_to raise_error
+        end
+      end
+
+      describe 'validate:' do
+        let(:schema) do
+          Meta::JsonSchema::SchemaBuilderTool.build validate: ->(value) {
+            raise Meta::JsonSchema::ValidationError, "value mustn't be zero"  if value == 0
+          }
+        end
+
+        it '作为自定义验证器' do
+          expect { schema.filter(0) }.to raise_error(Meta::JsonSchema::ValidationError)
+        end
+      end
+    end
+
+    describe 'ref:' do
+      let(:entity) do
+        Class.new(Meta::Entity) do
+          property :a
+          property :b
+        end
+      end
+
+      it '属性的过滤进一步交给 ref 的实体处理' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build ref: entity
+        value = schema.filter({ 'a' => 'a', 'b' => 'b', 'c' => 'c' })
+        expect(value).to eq({ a: 'a', b: 'b' })
+      end
+
+      it '属性的过滤进一步交给 ref 的实体处理（数组）' do
+        schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'array', ref: entity
+        value = schema.filter([{ 'a' => 'a', 'b' => 'b', 'c' => 'c' }])
+        expect(value).to eq([{ a: 'a', b: 'b' }])
+      end
+    end
+
+    describe 'dynamic_ref:' do
+      describe 'dynamic_ref.resolve:' do
+        it '动态获得实体' do
+          entity_a = Class.new(Meta::Entity) do
+            property :a
+          end
+          entity_b = Class.new(Meta::Entity) do
+            property :b
+          end
+          schema = Meta::JsonSchema::SchemaBuilderTool.build dynamic_ref: {
+            resolve: ->(object_value) {
+              object_value['name'] === 'a' ? entity_a : entity_b
+            }
+          }
+
+          value = schema.filter({ 'name' => 'a', 'a' => 'a', 'b' => 'b' })
+          expect(value).to eq(a: 'a')
+        end
+
+        context '同时设置其他选项' do
+          it '同时设置 `required: true` 时正常' do
+            inner_entity = Class.new(Meta::Entity) do
+              property :a
+            end
+            schema = Meta::JsonSchema::SchemaBuilderTool.build do
+              property :foo, required: true, dynamic_ref: { resolve: ->(value) { inner_entity } }
+            end
+
+            expect {
+              schema.filter({})
+            }.to raise_error(Meta::JsonSchema::ValidationErrors)
+          end
+
+          it '同时设置 `scope` 时正常' do
+            inner_entity = Class.new(Meta::Entity) do
+              property :a
+            end
+            schema = Meta::JsonSchema::SchemaBuilderTool.build do
+              property :foo, scope: :foo, dynamic_ref: { resolve: ->(value) { inner_entity } }
+              property :bar, scope: :bar, dynamic_ref: { resolve: ->(value) { inner_entity } }
+            end
+
+            value = schema.filter({
+              'foo' => { 'a' => 'a', 'b' => 'b'  },
+              'bar' => { 'a' => 'a', 'b' => 'b' }
+            }, scope: [:bar])
+            expect(value).not_to be_key(:foo)
+            expect(value).to be_key(:bar)
+          end
+        end
+
+        context '放在 render 下' do
+          it '亦正常' do
+            entity_a = Class.new(Meta::Entity) do
+              property :a
+            end
+            entity_b = Class.new(Meta::Entity) do
+              property :b
+            end
+            schema = Meta::JsonSchema::SchemaBuilderTool.build do
+              property :foo, render: {
+                required: true,
+                dynamic_ref: { resolve: ->(value) {
+                  value['name'] === 'a' ? entity_a : entity_b
+                } }
+              }
+            end
+
+            value = schema.filter({ 'foo' => { 'name' => 'a', 'a' => 'a', 'b' => 'b' } }, stage: :render)
+            expect(value[:foo]).to eq(a: 'a')
+          end
+        end
+      end
+
+      describe 'dynamic_ref: Proc' do
+        it '等效于 dynamic_ref: { resolve: Proc }' do
+          entity_a = Class.new(Meta::Entity) do
+            property :a
+          end
+          entity_b = Class.new(Meta::Entity) do
+            property :b
+          end
+          schema = Meta::JsonSchema::SchemaBuilderTool.build do
+            property :foo, required: true, dynamic_ref: ->(value) {
+              value['name'] === 'a' ? entity_a : entity_b
+            }
+          end
+
+          value = schema.filter('foo' => { 'name' => 'a', 'a' => 'a', 'b' => 'b' })
+          expect(value[:foo]).to eq(a: 'a')
+        end
+      end
+
+      describe 'dynamic_using' do
+        it '是 dynamic_ref 的别名' do
+          entity_a = Class.new(Meta::Entity) do
+            property :a
+          end
+          entity_b = Class.new(Meta::Entity) do
+            property :b
+          end
+          schema = Meta::JsonSchema::SchemaBuilderTool.build do
+            property :foo, required: true, dynamic_using: ->(value) {
+              value['name'] === 'a' ? entity_a : entity_b
+            }
+          end
+
+          value = schema.filter('foo' => { 'name' => 'a', 'a' => 'a', 'b' => 'b' })
+          expect(value[:foo]).to eq(a: 'a')
+        end
+      end
+    end
   end
 end
--- spec/json_schema/schema_spec.rb
+++ spec/json_schema/schema_spec.rb
@@ -3,269 +3,105 @@
 require 'spec_helper'
 
 describe 'schema' do
-  context 'discard_missing' do
-    it '丢弃缺失的参数' do
-      schema = Meta::JsonSchema::SchemaBuilderTool.build do
-        param :foo, type: 'string'
-        param :bar, type: 'string'
-      end.to_schema
+  describe '.filter' do
+    describe 'user options' do
+      describe 'stage:' do
+        context 'is :render' do
+          it '过滤掉 render: false 的属性' do
+            schema = Meta::JsonSchema::SchemaBuilderTool.build do
+              property :a
+              property :b, param: false
+              property :c, render: false
+            end
 
-      filtered = schema.filter({ 'foo' => 'foo' }, discard_missing: true)
-      expect(filtered).to eq(foo: 'foo')
-    end
-  end
-
-  context '传递 nil 给数组参数' do
-    shared_examples '过滤 nil 得到 nil' do
-      specify do
-        expect(schema.filter(nil)).to be nil
-      end
-    end
-
-    context '定义标量数组' do
-      let(:schema) do
-        Meta::JsonSchema::SchemaBuilderTool.build type: 'array'
-      end
-
-      include_examples '过滤 nil 得到 nil'
-    end
-
-    context '定义对象数组' do
-      let(:schema) do
-        Meta::JsonSchema::SchemaBuilderTool.build type: 'array' do
-          property :foo
-          property :bar
+            value = schema.filter({ 'a' => 'a', 'b' => 'b', 'c' => 'c' }, stage: :render)
+            expect(value.keys).to eq([:a, :b])
+          end
         end
       end
 
-      include_examples '过滤 nil 得到 nil'
-    end
-  end
+      describe 'discard_missing:' do
+        context 'is true' do
+          it '丢弃缺失的字段' do
+            schema = Meta::JsonSchema::SchemaBuilderTool.build do
+              param :foo, type: 'string'
+              param :bar, type: 'string'
+            end.to_schema
 
-  context 'render 数组和对象时，应用到非 JSON 类型的值' do
-    # render 数组和对象时，应用到非 JSON 类型的值，它会把它们当作对应类型的值来看待。
-    # 对于对象，obj[:foo] 会调用 obj.foo 方法；对于数组，arr[index]、arr.length 和
-    # arr.each 都会对应地调用
-    it 'render 对象时，调用对象的方法' do
-      schema = Meta::JsonSchema::SchemaBuilderTool.build do
-        property :foo
-        property :bar
-      end
+            filtered = schema.filter({ 'foo' => 'foo' }, discard_missing: true)
+            expect(filtered).to eq(foo: 'foo')
+          end
+        end
 
-      obj = Object.new
-      def obj.foo; 'foo' end
-      def obj.bar; 'bar' end
+        context 'is false' do
+          it '保留缺失的字段' do
+            schema = Meta::JsonSchema::SchemaBuilderTool.build do
+              param :foo, type: 'string'
+              param :bar, type: 'string'
+            end.to_schema
 
-      expect(schema.filter(obj)).to eq(foo: 'foo', bar: 'bar')
-    end
-
-    it 'render 数组时，首先调用数组的 `to_a` 方法' do
-      schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'array'
-
-      arr = Object.new
-      def arr.to_a; [1, 2, 3] end
-
-      expect(schema.filter(arr)).to eq([1, 2, 3])
-    end
-  end
-
-  describe 'filter options' do
-    describe '自定义验证器' do
-      let (:schema) do
-        Meta::JsonSchema::SchemaBuilderTool.build validate: ->(value) {
-          raise Meta::JsonSchema::ValidationError, "value mustn't be zero"  if value == 0
-        }
-      end
-
-      it '验证失败时抛出异常' do
-        expect { schema.filter(0) }.to raise_error(Meta::JsonSchema::ValidationError)
+            filtered = schema.filter({ 'foo' => 'foo' }, discard_missing: false)
+            expect(filtered).to eq(foo: 'foo', bar: nil)
+          end
+        end
       end
     end
 
-    describe 'using Entity' do
-      context '使用单独的一个 Entity' do
-        it '成功使用外部 Entity 类' do
-          entity_class = Class.new(Meta::Entity) do
-            property :a
-            property :b
+    describe 'value' do
+      context 'value is nil' do
+        shared_examples '过滤 nil 得到 nil' do
+          specify do
+            expect(schema.filter(nil)).to be nil
+          end
+        end
+
+        context '定义标量数组' do
+          let(:schema) do
+            Meta::JsonSchema::SchemaBuilderTool.build type: 'array'
           end
 
-          schema = Meta::JsonSchema::SchemaBuilderTool.build do
-            property :foo, required: true, using: entity_class
+          include_examples '过滤 nil 得到 nil'
+        end
+
+        context '定义对象数组' do
+          let(:schema) do
+            Meta::JsonSchema::SchemaBuilderTool.build type: 'array' do
+              property :foo
+              property :bar
+            end
           end
 
+          include_examples '过滤 nil 得到 nil'
+        end
+      end
+
+      context 'value is not a Hash' do
+        let(:value) {
           obj = Object.new
           def obj.a; 'a' end
           def obj.b; 'b' end
-
-          expect { schema.filter('foo' => obj) }.not_to raise_error
-        end
-      end
-    end
-
-    describe 'render: false' do
-      it '不过滤 render 选项为 false 的属性' do
-        schema = Meta::JsonSchema::SchemaBuilderTool.build do
-          property :foo
-          property :bar, render: false
-        end
-
-        value = schema.filter({ 'foo' => 'foo', 'bar' => 'bar' }, stage: :render)
-        expect(value.keys).to eq([:foo])
-      end
-    end
-  end
-
-  describe 'default: 默认值' do
-    it '生成 boolean 类型的默认值' do
-      schema = Meta::JsonSchema::SchemaBuilderTool.build do
-        property :flag, type: 'boolean', default: false
-      end
-
-      value = schema.filter({})
-      expect(value).to eq({ flag: false })
-    end
-  end
-
-  describe 'format validator' do
-    it 'nil 值不会触发 format validator' do
-      schema = Meta::JsonSchema::SchemaBuilderTool.build do
-        property :number, type: 'string', format: /\d+/
-      end
-
-      expect {
-        schema.filter(number: nil)
-      }.not_to raise_error
-    end
-  end
-
-  describe 'ref' do
-    it '使用 ref: Entity' do
-      entity = Class.new(Meta::Entity) do
-        property :a
-        property :b
-      end
-      schema = Meta::JsonSchema::SchemaBuilderTool.build ref: entity.to_schema
-
-      value = schema.filter({ 'a' => 'a', 'b' => 'b', 'c' => 'c' })
-      expect(value).to eq({ a: 'a', b: 'b' })
-    end
-
-    it '数组' do
-      the_entity = Class.new(Meta::Entity) do
-        param :name
-        param :age
-      end
-      schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'array', ref: the_entity
-      value = [{ name: 'Jim', age: 18 }]
-      expect(schema.filter(JSON.parse(JSON.generate(value)))).to eq(value)
-    end
-  end
-
-  describe 'dynamic_ref' do
-    it '使用 dynamic_ref.resolve' do
-      entity_a = Class.new(Meta::Entity) do
-        property :a
-      end
-      entity_b = Class.new(Meta::Entity) do
-        property :b
-      end
-      schema = Meta::JsonSchema::SchemaBuilderTool.build dynamic_ref: {
-        resolve: ->(value) {
-          value['name'] === 'a' ? entity_a : entity_b
+          obj
         }
-      }
 
-      value = schema.filter({ 'name' => 'a', 'a' => 'a', 'b' => 'b' })
-      expect(value).to eq(a: 'a')
-    end
-
-    it '使用多态时 `required` 选项应起作用' do
-      inner_entity = Class.new(Meta::Entity) do
-        property :a
-      end
-      schema = Meta::JsonSchema::SchemaBuilderTool.build do
-        property :foo, required: true, dynamic_ref: { resolve: ->(value) { inner_entity } }
-      end
-
-      expect {
-        schema.filter({})
-      }.to raise_error(Meta::JsonSchema::ValidationErrors)
-    end
-
-    it '使用多态时 `scope` 选项应起作用' do
-      inner_entity = Class.new(Meta::Entity) do
-        property :a
-      end
-      schema = Meta::JsonSchema::SchemaBuilderTool.build do
-        property :foo, scope: :foo, dynamic_ref: { resolve: ->(value) { inner_entity } }
-        property :bar, scope: :bar, dynamic_ref: { resolve: ->(value) { inner_entity } }
-      end
-
-      value = schema.filter({
-        'foo' => { 'a' => 'a', 'b' => 'b'  },
-        'bar' => { 'a' => 'a', 'b' => 'b' }
-      }, scope: [:bar])
-      expect(value).not_to be_key(:foo)
-      expect(value).to be_key(:bar)
-    end
-
-    context 'using Proc' do
-      it '等效于 using: { resolve }' do
-        entity_a = Class.new(Meta::Entity) do
-          property :a
-        end
-        entity_b = Class.new(Meta::Entity) do
-          property :b
-        end
-        schema = Meta::JsonSchema::SchemaBuilderTool.build do
-          property :foo, required: true, dynamic_ref: ->(value) {
-            value['name'] === 'a' ? entity_a : entity_b
-          }
-        end
-
-        value = schema.filter('foo' => { 'name' => 'a', 'a' => 'a', 'b' => 'b' })
-        expect(value[:foo]).to eq(a: 'a')
-      end
-
-      context '放在 render 下也能生效' do
-        it '等效于 using: { resolve }' do
-          entity_a = Class.new(Meta::Entity) do
+        it '调用对象的方法获取值' do
+          schema = Meta::JsonSchema::SchemaBuilderTool.build do
             property :a
-          end
-          entity_b = Class.new(Meta::Entity) do
             property :b
           end
-          schema = Meta::JsonSchema::SchemaBuilderTool.build do
-            property :foo, render: {
-              required: true,
-              dynamic_ref: ->(value) {
-                value['name'] === 'a' ? entity_a : entity_b
-              }
-            }
-          end
-
-          value = schema.filter({ 'foo' => { 'name' => 'a', 'a' => 'a', 'b' => 'b' } }, stage: :render)
-          expect(value[:foo]).to eq(a: 'a')
+          expect(schema.filter(value)).to eq(a: 'a', b: 'b')
         end
       end
 
-      context 'dynamic_using' do
-        it '它是 dynamic_ref 的别名' do
-          entity_a = Class.new(Meta::Entity) do
-            property :a
-          end
-          entity_b = Class.new(Meta::Entity) do
-            property :b
-          end
-          schema = Meta::JsonSchema::SchemaBuilderTool.build do
-            property :foo, required: true, dynamic_using: ->(value) {
-              value['name'] === 'a' ? entity_a : entity_b
-            }
-          end
+      context 'value is not an array' do
+        let(:value) do
+          arr = Object.new
+          def arr.to_a; [1, 2, 3] end
+          arr
+        end
 
-          value = schema.filter('foo' => { 'name' => 'a', 'a' => 'a', 'b' => 'b' })
-          expect(value[:foo]).to eq(a: 'a')
+        it '调用 to_a 获取数组元素' do
+          schema = Meta::JsonSchema::SchemaBuilderTool.build type: 'array'
+          expect(schema.filter(value)).to eq([1, 2, 3])
         end
       end
     end
